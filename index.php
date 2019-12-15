a0480398148d8886daf9f187f3dc97dd0241247b
Matt Briney <mkbriney@gmail.com>
2019-12-15 18:12:49 -0500

    Adding IP lookup

--- index.php
+++ index.php
@@ -1,8 +1,13 @@
+<?php 
+	$realIP = file_get_contents("http://ipecho.net/plain");
+?>
+
 <html>
 	<head>
 		<title>Hello!</title>
 	</head>
 	<body>
 		<h1>Hello World</h1>
+		<p>IP: <?php echo $realIP ?>
 	</body>
 </html>
